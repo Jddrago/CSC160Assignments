9cb99c76405952d0d06c4f029e75143d84b35454
jddrago <jason@delorie.com>
2016-07-29 20:04:49 -0600

    Updated each of the tabs to meet requirements. Added custom converter for inverting values on left eye and eyebrow

--- Code/MeeCreator/MeeCreator/MainWindow.xaml.cs
+++ Code/MeeCreator/MeeCreator/MainWindow.xaml.cs
@@ -24,5 +24,48 @@ namespace MeeCreator
         {
             InitializeComponent();
         }
+
+        private void ComboItemRed_Selected(object sender, RoutedEventArgs e)
+        {
+            RightEye.Fill = ComboItemRed.Background;
+            LeftEye.Fill = ComboItemRed.Background;
+        }
+
+        private void ComboItemBlue_Selected(object sender, RoutedEventArgs e)
+        {
+            RightEye.Fill = ComboItemBlue.Background;
+            LeftEye.Fill = ComboItemBlue.Background;
+        }
+
+        private void ComboItemPurple_Selected(object sender, RoutedEventArgs e)
+        {
+            RightEye.Fill = ComboItemPurple.Background;
+            LeftEye.Fill = ComboItemPurple.Background;
+        }
+
+ 
+        }
+    public class InvertValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double)
+            {
+                double temp = (double)value;
+                temp *= -1;
+                value = temp;
+            }
+            return value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double)
+            {
+                double temp = (double)value;
+                temp *= -1;
+                value = temp;
+            }
+            return value;
+        }
     }
 }
